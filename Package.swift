510ef2c05de33f37658823c042eccbdb55d31dc9
Seregin Mikhail <_silo@mail.ru>
2025-12-04 17:46:20 +0300

    add db package

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.89.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.8.0"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.7.2"),
+        .package(url: "https://github.com/amattit/ar-db-documentation", branch: "main"),
     ],
     targets: [
         .executableTarget(
@@ -17,7 +18,8 @@ let package = Package(
             dependencies: [
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
-                .product(name: "Vapor", package: "vapor")
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "DBDocumentationKit", package: "ar-db-documentation"),
             ]
         ),
         .testTarget(name: "AppTests", dependencies: [
